0f5b4c99e44592e3fd6080cc24ac132b22fc5111
Mitrios19 <99819966+Mitrios19@users.noreply.github.com>
2025-02-01 14:34:12 +0700

    Добавил модели, permissions.py, обновил serializers.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+
+# {
+#    "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczODQ4MDU3NiwianRpIjoiMDU2YTEwNDBhYTllNDdjZTgxM2NhOTIzMTlmNTE5ZDkiLCJ1c2VyX2lkIjoxfQ.geaPkazJgvCIjP6ecMvsBakVA2oaU9k4ftPQx4CVZm0",
+#    "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM4NjUzMzc2LCJqdGkiOiI1M2M5MTg5OWViYmY0NGIzODc4MDk1OTQ5NzgzZWNiOCIsInVzZXJfaWQiOjF9.a4FvS6V7TvhcI6SDdNEc4ouNTAYEoXYANl0ma857Kn8"
+# }
 
 urlpatterns = [
+    path('v1/', include([
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('', include('djoser.urls.jwt')),
+
+
+    ]))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,39 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from posts.models import Post, Group, Comment, User
+from .serializers import (PostSerializer, UserSerializer,
+                          GroupSerializer, CommentSerializer)
+from rest_framework import permissions
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
